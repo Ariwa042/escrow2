d6ae1fe2db3de1ac6778dc065923f51dcacb2e93
Ariwa <116925898+Ariwa042@users.noreply.github.com>
2024-09-21 03:56:00 +0100

    Update manage.py

--- manage.py
+++ manage.py
@@ -3,6 +3,10 @@
 import os
 import sys
 
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'your_project_name.settings')
+application = get_wsgi_application()
 
 def main():
     """Run administrative tasks."""
